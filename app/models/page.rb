482efab50aaee59896351299e32572f324ec783b
Muneeb Ahmad <muneeb.ahmadb@gmail.com>
2025-01-28 19:54:17 +0500

    Scraping Web pages and storing Web scraper results in Active Record model

---
+++ app/models/page.rb
@@ -0,0 +1,21 @@
+class Page < ApplicationRecord
+  has_many :results
+
+  validates :url, presence: true
+  validates :check_type, presence: true
+  validates :selector, presence: true
+  validates :match_text, presence: {if: ->{ check_type == "text" }}
+
+  def run_check!
+    scraper = Scraper.new(url)
+    result = case check_type
+             when "text"
+               scraper.text(selector: selector).downcase == match_text.downcase
+             when "exists"
+               scraper.present?
+             when "not_exists"
+               !scraper.present?
+             end
+    results.create(success: result)
+  end
+end
